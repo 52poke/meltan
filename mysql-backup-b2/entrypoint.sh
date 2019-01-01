27d891593e8d02c8144472f77597862473f933e7
Mudkip <i@mudkip.me>
2019-01-01 23:51:51 +0800

    Prevent unfinished backup

--- mysql-backup-b2/entrypoint.sh
+++ mysql-backup-b2/entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+echo '[mysqldump]' > /root/.my.cnf
+echo 'user=root' >> /root/.my.cnf
+echo "password=$(cat /run/secrets/mysql_root)" >> /root/.my.cnf
+
 mkdir -p /root/.config/rclone/
 echo '[b2]' > /root/.config/rclone/rclone.conf
 echo 'type = b2' >> /root/.config/rclone/rclone.conf
@@ -7,7 +11,7 @@ echo "account = $(cat /run/secrets/b2_account)" >> /root/.config/rclone/rclone.c
 echo "key = $(cat /run/secrets/b2_key)" >> /root/.config/rclone/rclone.conf
 
 mkdir -p /root/backup/database/
-mysqldump -h mysql -u root -p$(cat /run/secrets/mysql_root) 52poke | gzip > /root/backup/database/52poke.sql.gz
-mysqldump -h mysql -u root -p$(cat /run/secrets/mysql_root) 52poke_bbs | gzip > /root/backup/database/52poke_bbs.sql.gz
-mysqldump -h mysql -u root -p$(cat /run/secrets/mysql_root) --ignore-table=52poke_wiki.objectcache 52poke_wiki | gzip > /root/backup/database/52poke_wiki.sql.gz
+mysqldump -h mysql -u root 52poke | gzip > /root/backup/database/52poke.sql.gz && \
+mysqldump -h mysql -u root 52poke_bbs | gzip > /root/backup/database/52poke_bbs.sql.gz && \
+mysqldump -h mysql -u root --ignore-table=52poke_wiki.objectcache 52poke_wiki | gzip > /root/backup/database/52poke_wiki.sql.gz && \
 rclone copy /root/backup/database b2:52poke-backup/database
