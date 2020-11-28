78ed7441c0b37a3795cacf3e504e0ec810af774e
Mudkip <i@mudkip.me>
2020-11-28 19:37:33 +0000

    Remove unused images

--- nfs-backup-b2/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-export B2_ACCOUNT_ID=$(cat /run/secrets/b2_account)
-export B2_ACCOUNT_KEY=$(cat /run/secrets/b2_key)
-export RESTIC_PASSWORD=$(cat /run/secrets/nfs_backup)
-
-restic -r b2:52poke-backup:nfs backup /srv/nfs
--- stunnel/entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-mkdir -p /etc/stunnel/certs
-cp /run/secrets/client.pem /etc/stunnel/certs
-c_rehash /etc/stunnel/certs
-stunnel /etc/stunnel/stunnel.conf
