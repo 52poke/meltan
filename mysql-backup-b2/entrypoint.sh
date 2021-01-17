0d7753cfdc83d2c0636ec3a03b3b82ba3cf1568e
Mudkip <i@mudkip.me>
2021-01-17 17:36:54 +0000

    Backup MySQL without locking tables

--- mysql-backup-b2/entrypoint.sh
+++ mysql-backup-b2/entrypoint.sh
@@ -14,7 +14,7 @@ mkdir -p /root/backup/database/
 databases=`mysql -h $MYSQL_HOST -u root -e "SHOW DATABASES;" | grep -Ev "(Database|information_schema|performance_schema)"`
 
 for db in $databases; do
-  mysqldump -h $MYSQL_HOST -u root $MYSQLDUMP_OPT $db | gzip > /root/backup/database/$db.sql.gz
+  mysqldump --single-transaction -h $MYSQL_HOST -u root $MYSQLDUMP_OPT $db | gzip > /root/backup/database/$db.sql.gz
 done
 
 rclone copy /root/backup/database b2:$B2_TARGET
