8fe8cadd4f060d31ef3efe8644cbc05058009e22
Mudkip <i@mudkip.me>
2025-02-18 15:22:19 +0800

    Downgrade 52w-nginx

--- archived/52poke-remote-backup/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-DATE=`date "+%Y-%m-%dT%H_%M_%S"`
-
-rclone sync source:52poke-backup/database target:$BACKUP_DB_TARGET/back-$DATE
-rclone sync --exclude wiki/thumb/** --exclude wiki/temp/** --exclude webp-cache/** media:media.52poke.com target:$BACKUP_MEDIA_TARGET
-rclone sync source:52poke-backup/nfs target:$BACKUP_NFS_TARGET
--- archived/postgres-backup/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-set -e
-
-mkdir -p /root/backup/database/
-
-pg_dumpall -c -h $POSTGRES_HOST -p $POSTGRES_PORT -U postgres | gzip > /root/backup/database/postgres_backup.gz
-
-rclone copy /root/backup/database backup:$BACKUP_TARGET
